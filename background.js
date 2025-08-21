1dfe3ac5ae8bddef0764a18e993b128e10d5f122
lmn451 <putaluta@tuta.io>
2025-08-21 18:10:50 +0300

    prd

--- background.js
+++
@@ -1,7 +0,0 @@
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message.action === 'startRecording') {
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      chrome.tabs.sendMessage(tabs[0].id, { action: 'startRecording', source: message.source });
-    });
-  }
-});
